d5478c1f19b5b1d053f9766e05ffea6ee8df7666
Farid Maharramov <faridmaharramov@hotmail.com>
2022-10-13 14:53:56 +0300

    Simple Crud Template

---
+++ Lucene Net Example/Business/LuceneService.cs
@@ -0,0 +1,29 @@
+﻿using Lucene_Net_Example.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lucene_Net_Example.Business
+{
+    public class LuceneService
+    {
+        public async Task<List<Person>> SelectData(string searchKey)
+        {
+            List<Person> list = new List<Person>();
+            return list;
+        }
+        public bool InsertData(List<Person> person)
+        {
+            return true;
+        }
+
+        public bool UpdateData(List<Person> person)
+        {
+            return true;
+        }
+
+        public bool DeleteData(string name)
+        {
+            return true;
+        }
+    }
+}
---
+++ Lucene Net Example/Controllers/LuceneController.cs
@@ -0,0 +1,43 @@
+﻿using Lucene_Net_Example.Business;
+using Lucene_Net_Example.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Lucene_Net_Example.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LuceneController : ControllerBase
+    {
+        LuceneService _luceneService = new();
+        [HttpGet]
+        public async Task<IActionResult> Select(string searchKey)
+        {
+            var result = await _luceneService.SelectData(searchKey);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public IActionResult Insert(List<Person> people)
+        {
+            var result = _luceneService.InsertData(people);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public IActionResult Update(List<Person> people)
+        {
+            var result = _luceneService.UpdateData(people);
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(string Name)
+        {
+            var result = _luceneService.DeleteData(Name);
+            return Ok(result);
+        }
+    }
+}
