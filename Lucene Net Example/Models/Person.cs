315f875354c90960d4532a10a7bc6361455da875
Farid Maharramov <faridmaharramov@hotmail.com>
2022-10-13 15:34:05 +0300

    Insert Datas

--- Lucene Net Example/Models/Person.cs
+++ Lucene Net Example/Models/Person.cs
@@ -6,6 +6,6 @@
         public string Lastname { get; set; }
         public string Country { get; set; }
         public int Age { get; set; }
-        public bool Gender { get; set; }
+        public string Gender { get; set; }
     }
 }
